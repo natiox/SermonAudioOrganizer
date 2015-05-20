3ddc4eb8e25c0b8a80d479baa3b73bf002cc15b5
natiox <nalter@gmail.com>
2015-05-19 22:24:30 -0600

    Changed tables to bootstrap tables.  Fixed navbarNeed to get mediascan to connect to localdb.

---
+++ SermonAudioOrganizer.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SermonAudioOrganizer.Startup))]
+namespace SermonAudioOrganizer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
